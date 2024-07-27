b9c62244471ff24ede38db2cf34e1f5ff2ffca3d
o21ex <154024318+c2-tlhah@users.noreply.github.com>
2024-07-27 23:36:19 +0500

    Update hero.ts

--- src/lib/content/hero.ts
+++ src/lib/content/hero.ts
@@ -6,7 +6,7 @@ export const heroSection: HeroSectionType = {
     title: ['Muhammad Talha!', 'Big Data Engineer', 'Solutions Architect', 'Data Scientist'],
     tagline: 'I make ETL pipelines and Solutions Architecture.',
     description:
-    "I’m a Final Year Computer Science & Artifical Intelligence student at FAST National University of Computer and Emerging Sciences, focused on Big Data Technologies Stack and Machine Learnign. I enjoy learning new skills and building projects to gain hands on experince. I’m eager to grow and work well with others to turn ideas into reality.",
+    "I’m a Computer Science & Artifical Intelligence student at FAST National University of Computer and Emerging Sciences, focused on Big Data Technologies Stack and Machine Learning. I enjoy learning new skills and building projects to gain hands on experince. I’m eager to grow and work well with others to turn ideas into reality.",
     specialText: 'Currently available for job opportunities and freelance projects.',
     cta: {
         title: 'View My Resume',
